4805df2f9c7f6f098aa37adb04bafb145935bd10
tutmosisII <alejandroleon09@gmail.com>
2015-08-06 21:17:32 -0500

    Se agraga el concepto de Tablero y su prueba

---
+++ src/main/java/co/edu/uelbosque/sistemas/swii/c1/marte/Tablero.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uelbosque.sistemas.swii.c1.marte;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Tablero {
+
+    private int x;
+    private int y;
+
+    public Tablero(){
+        
+    }
+    public Tablero(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void setX(int x) {
+        this.x = x;
+
+    }
+
+    void setY(int y) {
+        this.y = y;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj) return true;
+        if(!(obj instanceof Tablero))
+            return false;
+        Tablero t=(Tablero)obj;
+        if(this.x==t.x && this.y==t.y)
+            return true;
+        return false;
+    }
+    
+    
+
+}
